4c750d1804b539353d09825c2dc67986cf2ed229
ryaremenko <ryaremenko@alliancedigital.tech>
2025-04-19 10:07:25 +0300

    upgraded version

--- src/main/java/com/sit/uapki/Library.java
+++ src/main/java/com/sit/uapki/Library.java
@@ -107,9 +107,9 @@ public final class Library {
             LoaderLibFromResource loader = new LoaderLibFromResource();
 
             //  Warning: here need order
-            loader.load("uapkic", "2.0.12");
-            loader.load("uapkif", "2.0.12");
-            loader.load("uapki", null);
+            loader.load("uapkic", "2.0.1");
+            loader.load("uapkif", "2.0.1");
+            loader.load("uapki", "2.0.12");
             
             //  Load cm-providers
             final String[] CM_LIBNAMES = new String[] {
